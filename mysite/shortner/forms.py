3f203e0d1fe6549d93d9e6ff99faafb9a7ad8b80
Aditya Pawar <adityapawar2019@gmail.com>
2021-09-02 14:02:43 +0530

    Added the shortner form

---
+++ mysite/shortner/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Shortner
+
+
+class ShortenerForm(ModelForm):
+    long_url = forms.URLField(widget=forms.URLInput(
+        attrs={"class": "form-control form-control-lg","placeholder":"Your URL to shorten"}
+    ))
+
+
+    class Meta:
+        model = Shortner
+        fields = ('long_url')
--- mysite/shortner/views.py
+++ mysite/shortner/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import HttpResponse
+
+def HelloWorld(request):
+    return HttpResponse("Helllo World")
